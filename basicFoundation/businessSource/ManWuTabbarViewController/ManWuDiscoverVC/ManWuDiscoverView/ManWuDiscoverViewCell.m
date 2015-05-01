b07609656f088724a3d10bfdd94ff1e8ac1c71bb
逸行 <xixi.mengxx@alibaba-inc.com>
2015-05-01 13:22:09 +0800

    增加发现的cellview，测试分支是否正确

---
+++ basicFoundation/businessSource/ManWuTabbarViewController/ManWuDiscoverVC/ManWuDiscoverView/ManWuDiscoverViewCell.m
@@ -0,0 +1,21 @@
+//
+//  ManWuDiscoverViewCell.m
+//  basicFoundation
+//
+//  Created by 逸行 on 15-5-1.
+//  Copyright (c) 2015年 逸行. All rights reserved.
+//
+
+#import "ManWuDiscoverViewCell.h"
+
+@implementation ManWuDiscoverViewCell
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
