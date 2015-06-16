9338e134731eafc3233a9a08e9cc87c398e7a3ea
许学 <xuxuezjut@126.com>
2015-06-16 20:21:30 +0800

    新建红包Cell

---
+++ basicFoundation/businessSource/ManWuTabbarViewController/ManMyInfoVC/common/KSRedPacketTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  KSUserInfoTaableViewCellTableViewCell.h
+//  basicFoundation
+//
+//  Created by 许学 on 15/6/14.
+//  Copyright (c) 2015年 逸行. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KSUserInfoTaableViewCellTableViewCell : UITableViewCell
+
+@end
---
+++ basicFoundation/businessSource/ManWuTabbarViewController/ManMyInfoVC/common/KSRedPacketTableViewCell.m
@@ -0,0 +1,23 @@
+//
+//  KSUserInfoTaableViewCellTableViewCell.m
+//  basicFoundation
+//
+//  Created by 许学 on 15/6/14.
+//  Copyright (c) 2015年 逸行. All rights reserved.
+//
+
+#import "KSUserInfoTaableViewCellTableViewCell.h"
+
+@implementation KSUserInfoTaableViewCellTableViewCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
